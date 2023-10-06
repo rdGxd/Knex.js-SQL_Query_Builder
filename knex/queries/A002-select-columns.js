fcb12e4a00391c402f558a69eda8b9448f355ef7
Rodrigo <rdg.hora1@gmail.com>
2023-10-06 10:51:32 -0300

    Update A002-select-columns.js

--- knex/queries/A002-select-columns.js
+++ knex/queries/A002-select-columns.js
@@ -9,7 +9,6 @@ const knex = require("../config/database.js");
 const select = knex("users as u").select("u.email as uemail", "u.id as uid");
 
 console.log(select.toString());
-// console.log(select.toSQL().toNative());
 
 select
   .then((data) => {
