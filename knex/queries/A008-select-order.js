903c933d1ebad3bde0a88293de59ac88fce091a4
Rodrigo <rdg.hora1@gmail.com>
2023-10-08 09:06:16 -0300

    A008-select-order.js

--- knex/queries/A008-select-order.js
+++ knex/queries/A008-select-order.js
@@ -8,3 +8,24 @@ from users
 where id between 100 and 150
 order by first_name desc;
 */
+
+const knex = require("../config/database.js");
+const selectOrder = knex("users")
+  .select("id", "first_name")
+  .where("first_name", "like", "___")
+  .orWhere("first_name", "like", "%na")
+  .orderBy("id", "desc")
+  .orderBy("first_name", "asc");
+
+console.log(selectOrder.toString());
+
+selectOrder
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
--- knex/queries/A009-select-limit.js
+++ knex/queries/A009-select-limit.js
@@ -7,3 +7,24 @@ where id between 100 and 150
 order by id asc
 limit 9,3;
 */
+
+const knex = require("../config/database.js");
+const selectOrder = knex("users")
+  .select("id", "first_name")
+  .where("first_name", "like", "___")
+  .orWhere("first_name", "like", "%na")
+  .orderBy("id", "desc")
+  .orderBy("first_name", "asc");
+
+console.log(selectOrder.toString());
+
+selectOrder
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
