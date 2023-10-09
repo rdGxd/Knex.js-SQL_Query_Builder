5a829bd5f3dfda67c5ae4afad601bac32c979931
Rodrigo <rdg.hora1@gmail.com>
2023-10-09 10:17:00 -0300

    Delete no Knex

---
+++ knex/queries/A011-delete.js
@@ -0,0 +1,34 @@
+/*
+-- Delete apaga registros da tabela
+DELETE FROM users where id = 115;
+
+-- Aviso: use SELECT para garantir que está
+-- apagando os valores corretos
+select * from users where id BETWEEN 110 and 115;
+*/
+
+const knex = require("../config/database");
+const select = knex("users").select("id", "first_name");
+const deleteSql = knex("users").delete().whereBetween("id", [101, 104]);
+
+console.log(select.toString());
+console.log(deleteSql.toString());
+
+deleteSql
+  .then((data) => {
+    console.log(data);
+
+    select
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  })
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
