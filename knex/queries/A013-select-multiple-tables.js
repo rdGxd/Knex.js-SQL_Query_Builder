d1645223b3e9b025aaab0d865db981651f18b8d2
Rodrigo <rdg.hora1@gmail.com>
2023-10-10 10:35:17 -0300

    Select de varias tabelas

--- knex/queries/A013-select-multiple-tables.js
+++ knex/queries/A013-select-multiple-tables.js
@@ -11,3 +11,21 @@ WHERE u.id = p.user_id;
  */
 
 const knex = require("../config/database");
+const select = knex(
+  knex.raw("?? as ??, ?? as ??", ["users", "u", "profiles", "p"])
+)
+  .select("u.id as uid", "p.id as pid", "p.bio", "u.first_name")
+  .where({ "u.id": knex.raw("??", ["p.user_id"]) });
+
+// console.log(select.toString());
+
+select
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
