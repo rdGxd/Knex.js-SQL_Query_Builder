b24a184805b67b452919a8ec57ab301b9985afaf
Rodrigo <rdg.hora1@gmail.com>
2023-10-12 10:35:19 -0300

    Update com Round e Rand no Knex

--- knex/queries/A017-update-rand-round.js
+++ knex/queries/A017-update-rand-round.js
@@ -6,3 +6,20 @@ select salary from users where
 salary BETWEEN 1000 and 1500
 order by salary asc;
 */
+
+const knex = require("../config/database");
+const update = knex("users").update({
+  // salary: knex.raw("round(rand() * 10000, 2)"),
+  salary: knex.raw("round(rand() * ??, ??)", [10000, 2]), // Se vc quiser receber de algum usuário utilize dessa forma
+});
+
+update
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
