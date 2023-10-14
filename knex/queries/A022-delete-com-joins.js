f8bd85d70985c17a019d605bffc15f280af595b1
Rodrigo <rdg.hora1@gmail.com>
2023-10-14 10:25:37 -0300

    Delete com join no Knex

--- knex/queries/A022-delete-com-joins.js
+++ knex/queries/A022-delete-com-joins.js
@@ -10,3 +10,27 @@ left join profiles as p
 on p.user_id = u.id
 where u.first_name = 'Katelyn';
 */
+
+const knex = require("../config/database");
+const deleteSql = knex.raw(
+  `
+  delete p, u from users u
+  inner join profiles as p
+  on p.user_id = u.id
+  where u.first_name = ? and p.bio like ?;
+`,
+  ["Katelyn", "%Katelyn%"]
+);
+
+console.log(deleteSql.toString());
+
+deleteSql
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(() => {
+    knex.destroy();
+  });
